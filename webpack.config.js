808877d9c4063df7c916492e2dbf80d4e85b5e3b
Serg Tereshchenko <serg.partizan@gmail.com>
2021-03-03 13:08:56 +0200

    chore: Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
-const path = require("path");
-
-module.exports = function (env, argv={}) {
-  return {
-    mode: "development",
-    target: "node",
-    entry: "./src/main.js",
-    module: {
-      rules: [
-        {
-          test: /\.(json5?|ya?ml)$/, // target json, json5, yaml and yml files
-          type: "javascript/auto",
-          loader: "@intlify/vue-i18n-loader",
-        },
-      ],
-    },
-  }
-}
+module.exports = {
+  mode: "development",
+  target: "web",
+  entry: "./src/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.(json5?|ya?ml)$/, // target json, json5, yaml and yml files
+        type: "javascript/auto",
+        loader: "@intlify/vue-i18n-loader",
+      },
+    ],
+  },
+};
